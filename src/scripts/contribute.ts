af4e7e4f25207d359de8326dfcaa304752a491be
Miguel Palhas <mpalhas@gmail.com>
2020-03-18 23:35:53 +0000

    Fixes contribution command

--- src/scripts/contribute.ts
+++ src/scripts/contribute.ts
@@ -6,28 +6,11 @@
 
 import * as _ from "lodash";
 
-import { react, reply, getChannels } from '../slack';
+import { react, reply } from '../slack';
 
 module.exports = (robot: any) => {
   robot.respond(/can I contribute to this bot?/i, async (res: any) => {
     react(res, 'heavy_check_mark');
     reply(res, "Sure thing!\nSource code is at https://github.com/subvisual/tech4covid-slack-bot\nPing Miguel Palhas in this Slack, he'll give you access.")
-    const prefix = res.match[1].replace(/s$/, '');
-
-    const resp: any = await getChannels();
-
-    const msg = _.chain(resp.channels).
-      map('name').
-      filter(name => name.match(new RegExp(`^${prefix}s?_`))).
-      map(name => `#${name}`).
-      value().
-      join('\n');
-
-    if (msg === '') {
-    reply(res, `No channels found with prefix ${prefix}`);
-    } else {
-      reply(res, msg);
-    }
   });
-
 };
