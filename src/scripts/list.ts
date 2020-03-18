622797c93f7f6018efb2c7f3f2104c53cf3333ae
Miguel Palhas <mpalhas@gmail.com>
2020-03-18 20:57:52 +0000

    List command

---
+++ src/scripts/list.ts
@@ -0,0 +1,31 @@
+// Description:
+//   Lists skills/projects/prospects/etc available in this Slack
+//
+// Commands:
+//   @bot list <prefix> - prefix can be skills/projects/prospects
+
+import * as _ from "lodash";
+
+import { reply, getChannels } from '../slack';
+
+module.exports = (robot: any) => {
+  robot.respond(/list (\w+)/i, async (res: any) => {
+    const prefix = res.match[1];
+
+    const resp: any = await getChannels();
+
+    const msg = _.chain(resp.channels).
+      map('name').
+      filter(name => name.match(new RegExp(`^${prefix}s?_`))).
+      map(name => `#${name}`).
+      value().
+      join('\n');
+
+    if (msg === '') {
+    reply(res, `No channels found with prefix ${prefix}`);
+    } else {
+      reply(res, msg);
+    }
+  });
+
+};
