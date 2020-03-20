982449dc17a6573b284eb8e75e830680adf65dfa
Miguel Palhas <mpalhas@gmail.com>
2020-03-20 18:01:52 +0000

    Using msg from sheet cell

--- src/jobs/daily_notify.ts
+++ src/jobs/daily_notify.ts
@@ -4,14 +4,13 @@ import { google } from 'googleapis';
 
 const usernames = ["@naps62_Dev-DevOps_Subvisual"];
 
-const Link = "https://humaniaks.typeform.com/to/pjr0jh";
-const Message = `Olá! Para melhor ajudar os projetos, pedidos updates a cada 24h. Podes por favor <${Link}|dar-nos um update aqui>. Obrigado.`;
+const SheetID="1rJqTqH-QpFGlHWdyXhRX1Oe5_5NI9Dts5uESzQk7r7I";
+
+const googleClient = google.sheets({version: 'v4', auth: process.env.GOOGLE_API_KEY});
 
 const getUsernames = async () => {
-  const client = google.sheets({version: 'v4', auth: process.env.GOOGLE_API_KEY});
-
-  const res: any = await client.spreadsheets.values.get({
-    spreadsheetId: '1rJqTqH-QpFGlHWdyXhRX1Oe5_5NI9Dts5uESzQk7r7I',
+  const res: any = await googleClient.spreadsheets.values.get({
+    spreadsheetId: SheetID,
     range: 'E3:E500',
   })
 
@@ -22,19 +21,13 @@ const getUsernames = async () => {
     value();
 }
 
-const getMessage = async() => {
-  const client = google.sheets({version: 'v4', auth: process.env.GOOGLE_API_KEY});
-
-  const res: any = await client.spreadsheets.values.get({
-    spreadsheetId: '1rJqTqH-QpFGlHWdyXhRX1Oe5_5NI9Dts5uESzQk7r7I',
-    range: 'E3:E500',
+const getMessage = async () => {
+  const res: any = await googleClient.spreadsheets.values.get({
+    spreadsheetId: SheetID,
+    range: 'Dailies!B1:B1',
   })
 
-  return _.chain(res.data.values).
-    map(row => row[0]).
-    filter(handle => !!handle).
-    uniq().
-    value();
+  return res.data.values[0][0];
 }
 
 const allSlackUsers = async(api: any) => {
@@ -60,13 +53,14 @@ const findSlackUserId = async (name: string, slackUsers: any[]): Promise<any> =>
     return r;
   }
 
-  console.log(`not found ${name}`);
+  console.log(`Slack Handle not found for ${name}`);
   return null;
 }
 
 (async()=>{
   try {
     const api = new WebClient(process.env.HUBOT_SLACK_TOKEN);
+    const msg = await getMessage();
     const slackUsers = await allSlackUsers(api);
 
     const realUsers: any = await Promise.all(
@@ -90,7 +84,7 @@ const findSlackUserId = async (name: string, slackUsers: any[]): Promise<any> =>
     _.map(users, async (user:any)=>{
       try {
         const dm: any = await api.conversations.open({users: `${user.id}`});
-        await api.chat.postMessage({text: Message, channel: dm.channel.id});
+        await api.chat.postMessage({text: msg, channel: dm.channel.id});
       } catch(err) {
         console.log(err)
       }
